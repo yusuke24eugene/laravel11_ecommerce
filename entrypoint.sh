d89b34bb66239d1b26f5aab8d975edfd20e5812b
PepeSmith <pepe.smith@email.com>
2025-06-04 08:17:55 +0800

    removed php artisan migrate:fresh in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,6 @@ php artisan config:clear
 php artisan route:clear
 php artisan config:cache
 php artisan route:cache
-php artisan migrate:fresh
 php artisan migrate --force
 
 # Start the Laravel app
